8e152e8b99cf3826ede75f23842e58ef0f3372f9
XiuNya <1027656813@qq.com>
2020-05-16 23:51:22 +0800

    Bilibili P27 USART Part2

--- Template/USER/main.c
+++ Template/USER/main.c
@@ -7,59 +7,40 @@
 #include "beep.h"
 #include "key.h"
 
-void My_USART1_Init(void)
-{
-	GPIO_InitTypeDef  GPIO_InitStructure;
-	USART_InitTypeDef  USART_InitStructure;
-	NVIC_InitTypeDef  NVIC_InitStructure;
-	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);//Ê¹ÄÜUSART1Ê±ÖÓ
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
-	
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_USART1);
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource10,GPIO_AF_USART1);
-	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	USART_InitStructure.USART_BaudRate=115200;
-	USART_InitStructure.USART_HardwareFlowControl=USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode=USART_Mode_Rx|USART_Mode_Tx;
-	USART_InitStructure.USART_Parity=USART_Parity_No;
-	USART_InitStructure.USART_StopBits=USART_StopBits_1;
-	USART_InitStructure.USART_WordLength=USART_WordLength_8b;
-	USART_Init(USART1,&USART_InitStructure);
-	USART_Cmd(USART1,ENABLE);
-	
-	USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
-	
-	NVIC_InitStructure.NVIC_IRQChannel=USART1_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority=1;
-	NVIC_Init(&NVIC_InitStructure);
-
-}
-
-void USART1_IRQHandler(void)
-{
-	u8 res;
-	if(USART_GetITStatus(USART1,USART_IT_RXNE))
-	{
-		res=USART_ReceiveData(USART1);
-		USART_SendData(USART1,res);
-	}
-
-}
-	
 int main(void)
 {
+	u8 t;
+	u8 len;	
+	u16 times=0;  
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	My_USART1_Init();
-	while(1);
+	delay_init(168);
+	uart_init(115200);
+	LED_Init();
+	while(1)
+	{
+		if(USART_RX_STA&0x8000)
+		{					   
+			len=USART_RX_STA&0x3fff;
+			printf("\r\nThe message you sent:\r\n");
+			for(t=0;t<len;t++)
+			{
+				USART_SendData(USART1, USART_RX_BUF[t]);
+				while(USART_GetFlagStatus(USART1,USART_FLAG_TC)!=SET);
+			}
+			printf("\r\n\r\n");
+			USART_RX_STA=0;
+		}else
+		{
+			times++;
+			if(times%5000==0)
+			{
+				printf("\r\nSTM32F407 USART\r\n");
+				printf("learn by yourself\r\n\r\n\r\n");
+			}
+			if(times%200==0)printf("Please enter the data and end with the Enter key\r\n");  
+			if(times%30==0)LED0=!LED0;//??LED,????????.
+			delay_ms(10);   
+		}
+	}
 	
 }
